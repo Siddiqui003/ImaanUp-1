9c59cf8e8ea51f474705ed4b77ca42566c009481
noman-faraz-islamic <nomanfaraz.islamic@gmail.com>
2024-11-10 12:48:42 +0530

    basic quiz with react and sliding effect and gitignore files

---
+++ react-basic-quiz/src/components/Home.js
@@ -0,0 +1,19 @@
+// src/components/Home.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Navbar from './Navbar';
+
+const Home = () => {
+    return (
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+            <Navbar/>
+            <h1>Welcome to Eemaan Up</h1>
+            <p>Test your knowledge with our quiz!</p>
+            <Link to="/quiz">
+                <button style={{ padding: '10px 20px', fontSize: '1rem' }}>Start Quiz</button>
+            </Link>
+        </div>
+    );
+};
+
+export default Home;
---
+++ react-basic-quiz/src/components/Navbar.js
@@ -0,0 +1,21 @@
+// src/components/Navbar.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="logo">
+                <img src="/logo.png" alt="Eemaan Up Logo" className="logo-image" />
+                <span className="app-name">Eemaan Up</span>
+            </div>
+            <div className="nav-links">
+                <Link to="/">Home</Link>
+                <Link to="/quiz">Quiz</Link>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
