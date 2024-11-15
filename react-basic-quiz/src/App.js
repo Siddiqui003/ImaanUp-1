723100e5a49bb85b2789410746f2cd973def081a
noman-faraz-islamic <nomanfaraz.islamic@gmail.com>
2024-11-15 16:13:02 +0530

    deployment changes

--- react-basic-quiz/src/App.js
+++ react-basic-quiz/src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import Quiz from './components/Quiz/Quiz';
-import QuranPlaylist from './components/Quran/Quran'
+import QuranPlaylist from './components/Quran/Quran';
 import FeedbackButton from './components/FeedbackButton/FeedbackButton';
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/quiz" component={Quiz} />
         <Route exact path="/quran-youtube" component={QuranPlaylist} />
       </Switch>
       <FeedbackButton />
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
--- react-basic-quiz/src/components/Navbar.js
+++ react-basic-quiz/src/components/Navbar.js
@@ -7,13 +7,13 @@ const Navbar = () => {
     return (
         <nav className="navbar">
             <Link to="/" className="navbar-logo">
-                <img src="/logo.png" alt="ImaanUp Logo" className="logo-image" />
+            <img 
+                src={ process.env.REACT_APP_MODE === 'local'? "ImaanUp/logo.png": "logo.png" }
+                alt="ImaanUp Logo" 
+                className="logo-image" 
+            />
                 <span className="app-name">ImaanUp</span>
             </Link>
-            <div className="navbar-links">
-                <Link to="/" className="nav-link">Home</Link>
-                <Link to="/quiz" className="nav-link">Quiz</Link>
-            </div>
         </nav>
     );
 };
