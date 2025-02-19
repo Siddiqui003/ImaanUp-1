dd9fb3ed1f5d67feea80a45ff5389aa86b4580af
Siddiqui003 <mohtasimroxxthebest333@gmail.com>
2025-02-19 19:42:25 +0530

    changed background, added effects to navbar

--- react-basic-quiz/src/components/Navbar.js
+++ react-basic-quiz/src/components/Navbar.js
@@ -1,21 +1,36 @@
 // src/components/Navbar.js
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
-    return (
-        <nav className="navbar">
-            <Link to="/" className="navbar-logo">
-            <img 
-                src={ process.env.REACT_APP_MODE === 'local'? "ImaanUp/logo.png": "logo.png" }
-                alt="ImaanUp Logo" 
-                className="logo-image" 
-            />
-                <span className="app-name">ImaanUp</span>
-            </Link>
-        </nav>
-    );
+  return (
+    <nav className="navbar">
+      <div className="nav-sections">
+        <Link to="/" className="navbar-logo">
+          <img
+            src={
+              process.env.REACT_APP_MODE === "local"
+                ? "ImaanUp/logo.png"
+                : "logo.png"
+            }
+            alt="ImaanUp Logo"
+            className="logo-image"
+          />
+        </Link>
+        <span className="app-name">ImaanUp</span>
+        <Link to="/about" className="nav-links">
+          About Us
+        </Link>
+        <Link to="/contact" className="nav-links">
+          Contact Us
+        </Link>
+        <Link to="/contribute" className="nav-links">
+          Contribute
+        </Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
--- react-basic-quiz/src/components/Quran/Quran.js
+++ react-basic-quiz/src/components/Quran/Quran.js
@@ -1,19 +1,30 @@
-import React, { useState, useEffect,useCallback } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import Modal from "react-modal";
 import Spinner from "../Spinner/Spinner";
-import './Quran.css';
+import "./Quran.css";
 import Navbar from "../Navbar";
 
-
-const apiKey = process.env.REACT_APP_YOUTUBE_API_KEY // Replace with your actual YouTube API key
-
+const apiKey = process.env.REACT_APP_YOUTUBE_API_KEY; // Replace with your actual YouTube API key
 
 // Qaari map with display name and playlist IDs
 const qariMap = {
-  mishary: { displayName: "Mishary Rashid Alafasy", playlistId: "PLoqNzfHlA__knCeUoKUHjQfZpUL6mj64w" },
-  saad: { displayName: "Saad Al-Ghamdi", playlistId: "PLhM2xiAUdw2cAqW_o3zZkbhJNw0bnaBZN" },
-  sudais: { displayName: "Imam Sudais" , playlistId: "PLwGJCASZfx4OKFE2TjvBhiNKUyfLa8f6B"},
-  maher: { displayName: "Maher Al-Muaiqly", playlistId: "PL96519CC4BF5DD993"}
+  mishary: {
+    displayName: "Mishary Rashid Alafasy",
+    playlistId: "PLoqNzfHlA__knCeUoKUHjQfZpUL6mj64w",
+  },
+  saad: {
+    displayName: "Saad Al-Ghamdi",
+    playlistId: "PLhM2xiAUdw2cAqW_o3zZkbhJNw0bnaBZN",
+  },
+  sudais: {
+    displayName: "Imam Sudais",
+    playlistId: "PLwGJCASZfx4OKFE2TjvBhiNKUyfLa8f6B",
+  },
+  maher: { displayName: "Maher Al-Muaiqly", playlistId: "PL96519CC4BF5DD993" },
+  shuraim: {
+    displayName: "Saad ash-Shuraim",
+    playlistId: "PLzX-2km3_XsTt50tYP0s67dA_kipr35O_",
+  },
 };
 
 const QuranComponent = () => {
@@ -27,46 +38,48 @@ const QuranComponent = () => {
   const [selectedQari, setSelectedQari] = useState("mishary"); // Default Qaari selection
   const itemsPerPage = 12;
 
-  
-
   // Fetch videos for the selected Qaari's playlist
-  const fetchVideos = useCallback(async (playlistId) => {
-    setIsLoading(true);
-    let allVideos = [];
-    let nextPageToken = '';
-  
-    try {
-      do {
-        const response = await fetch(
-          `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${playlistId}&maxResults=50&pageToken=${nextPageToken}&key=${apiKey}`
-        );
-        const data = await response.json();
-  
-        allVideos = [...allVideos, ...data.items];
-        nextPageToken = data.nextPageToken || '';
-      } while (nextPageToken);
-  
-      // Apply search term filter if there's a search term
-      const filteredVideos = searchTerm
-        ? allVideos.filter(video =>
-            video.snippet.title.toLowerCase().includes(searchTerm.toLowerCase())
-          )
-        : allVideos;
-  
-      setVideoList(allVideos);
-      setFilteredVideos(filteredVideos);
-      setCurrentPage(1);
-    } catch (error) {
-      console.error("Error fetching videos:", error);
-    } finally {
-      setIsLoading(false);
-    }
-  },[searchTerm])
+  const fetchVideos = useCallback(
+    async (playlistId) => {
+      setIsLoading(true);
+      let allVideos = [];
+      let nextPageToken = "";
+
+      try {
+        do {
+          const response = await fetch(
+            `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${playlistId}&maxResults=50&pageToken=${nextPageToken}&key=${apiKey}`
+          );
+          const data = await response.json();
+
+          allVideos = [...allVideos, ...data.items];
+          nextPageToken = data.nextPageToken || "";
+        } while (nextPageToken);
+
+        // Apply search term filter if there's a search term
+        const filteredVideos = searchTerm
+          ? allVideos.filter((video) =>
+              video.snippet.title
+                .toLowerCase()
+                .includes(searchTerm.toLowerCase())
+            )
+          : allVideos;
+
+        setVideoList(allVideos);
+        setFilteredVideos(filteredVideos);
+        setCurrentPage(1);
+      } catch (error) {
+        console.error("Error fetching videos:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    },
+    [searchTerm]
+  );
 
   useEffect(() => {
     fetchVideos(qariMap[selectedQari].playlistId);
-  }, [selectedQari,searchTerm,fetchVideos]);
-  
+  }, [selectedQari, searchTerm, fetchVideos]);
 
   // Handle Qaari selection change
   const handleQariChange = (e) => {
@@ -82,7 +95,7 @@ const QuranComponent = () => {
       setFilteredVideos(videoList);
       setCurrentPage(1);
     } else {
-      const matchedVideos = videoList.filter(video =>
+      const matchedVideos = videoList.filter((video) =>
         video.snippet.title.toLowerCase().includes(term)
       );
       setFilteredVideos(matchedVideos);
@@ -100,32 +113,35 @@ const QuranComponent = () => {
   };
 
   // Open modal and set current video id
-    const openModal = (videoId) => {
-      setCurrentVideoId(videoId);
-      setIsModalOpen(true);
-    };
+  const openModal = (videoId) => {
+    setCurrentVideoId(videoId);
+    setIsModalOpen(true);
+  };
 
-    // Close modal
-    const closeModal = () => {
-      setIsModalOpen(false);
-      setCurrentVideoId(null);
-    };
-
+  // Close modal
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setCurrentVideoId(null);
+  };
 
   return (
     <div>
-      <Navbar/>
-    <div className="quran-container">
-      <h1 className="quran-header">Quranic Audios - { qariMap[selectedQari].displayName }
-
-      <select className="qari-dropdown" value={selectedQari} onChange={handleQariChange}>
-      {Object.keys(qariMap).map((qariKey) => (
-        <option key={qariKey} value={qariKey}>
-          {qariMap[qariKey].displayName}
-        </option>
-      ))}
-    </select>
-    </h1>
+      <Navbar />
+      <div className="quran-container">
+        <h1 className="quran-header">
+          Quranic Audios - {qariMap[selectedQari].displayName}
+          <select
+            className="qari-dropdown"
+            value={selectedQari}
+            onChange={handleQariChange}
+          >
+            {Object.keys(qariMap).map((qariKey) => (
+              <option key={qariKey} value={qariKey}>
+                {qariMap[qariKey].displayName}
+              </option>
+            ))}
+          </select>
+        </h1>
 
         <input
           type="text"
@@ -135,61 +151,65 @@ const QuranComponent = () => {
           onChange={handleSearch}
         />
 
-      {isLoading ? (
-        <Spinner />
-      ) : (
-        <>
-          <div className="video-thumbnails">
-            {currentItems.map((video, index) => (
-              <div
-                key={index}
-                className="video-thumbnail"
-                onClick={() => openModal(video.snippet.resourceId.videoId)}
-              >
-                <img
-                  src={video.snippet.thumbnails.medium.url}
-                  alt={video.snippet.title}
-                  className="thumbnail-image"
-                />
-                <p>{video.snippet.title}</p>
-              </div>
-            ))}
-          </div>
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <>
+            <div className="video-thumbnails">
+              {currentItems.map((video, index) => (
+                <div
+                  key={index}
+                  className="video-thumbnail"
+                  onClick={() => openModal(video.snippet.resourceId.videoId)}
+                >
+                  <img
+                    src={video.snippet.thumbnails.medium.url}
+                    alt={video.snippet.title}
+                    className="thumbnail-image"
+                  />
+                  <p>{video.snippet.title}</p>
+                </div>
+              ))}
+            </div>
 
-          <div className="pagination">
-            {Array.from({ length: pageCount }, (_, index) => (
-              <button
-                key={index}
-                className={`page-button ${index + 1 === currentPage ? 'active' : ''}`}
-                onClick={() => handlePageChange(index + 1)}
-              >
-                {index + 1}
-              </button>
-            ))}
-          </div>
-        </>
-      )}
+            <div className="pagination">
+              {Array.from({ length: pageCount }, (_, index) => (
+                <button
+                  key={index}
+                  className={`page-button ${
+                    index + 1 === currentPage ? "active" : ""
+                  }`}
+                  onClick={() => handlePageChange(index + 1)}
+                >
+                  {index + 1}
+                </button>
+              ))}
+            </div>
+          </>
+        )}
 
-      <Modal
-        isOpen={isModalOpen}
-        onRequestClose={closeModal}
-        contentLabel="Video Player"
-        className="video-modal"
-      >
-        <div className="modal-content">
-          <iframe
-            width="100%"
-            height="100%"
-            src={`https://www.youtube.com/embed/${currentVideoId}`}
-            title="YouTube video"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
-          <button className="close-button" onClick={closeModal}>&times;</button>
-        </div>
-      </Modal>
-    </div>
+        <Modal
+          isOpen={isModalOpen}
+          onRequestClose={closeModal}
+          contentLabel="Video Player"
+          className="video-modal"
+        >
+          <div className="modal-content">
+            <iframe
+              width="100%"
+              height="100%"
+              src={`https://www.youtube.com/embed/${currentVideoId}`}
+              title="YouTube video"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+            <button className="close-button" onClick={closeModal}>
+              &times;
+            </button>
+          </div>
+        </Modal>
+      </div>
     </div>
   );
 };
